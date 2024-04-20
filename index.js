bb94aecc9dc45f4527177fd62b678791d83bf881
Hans How <hanshow00@gmail.com>
2024-04-19 19:34:18 -0500

    dom elements lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+// Remove the main element with id 'main'
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+// Create a new h1 element
+const newHeader = document.createElement('h1');
+
+// Assign id 'victory' to newHeader
+newHeader.setAttribute('id', 'victory');
+
+// Set text content of newHeader
+const yourName = "Hans"; // Replace 'Your Name' with your actual name
+newHeader.textContent = `${yourName} is the champion`;
+
+// Append newHeader to the body
+document.body.appendChild(newHeader);
